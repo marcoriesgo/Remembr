988cc983379a4aa376526274ad7a3075d870f7a3
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-03-11 17:23:18 -0700

    fixed API for heroku

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -13,11 +13,6 @@ $('#generateQuote').click(function() {
       .fail(handleErr);
 });
 
-// $('#generateQuote').click(function() {
-//   $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
-//     .done(update)
-//     .fail(handleErr);
-// });
 
 function update(response) {
     $('#quote').empty();
